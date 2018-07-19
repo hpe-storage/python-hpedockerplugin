4342269238f3914c2a67ac5ee39a9c2b97db33a6
nilangekarss <swapnil.sun.nilangekar@hpe.com>
2018-07-18 23:25:21 -0600

    corrected variable name of snapshotPrefix

--- hpedockerplugin/hpe_storage_api.py
+++ hpedockerplugin/hpe_storage_api.py
@@ -343,7 +343,7 @@ class VolumePlugin(object):
                             return response
 
                 if 'scheduleName' not in contents['Opts'] or \
-                                'schedPrefix' not in contents['Opts']:
+                                'snaphotPrefix' not in contents['Opts']:
                     msg = ('Please make sure that valid schedule name is passed '
                            'or please provide a 3 letter prefix for this schedule ')
                     LOG.info(msg)
