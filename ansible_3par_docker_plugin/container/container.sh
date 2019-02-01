6c84f7b2e203f4295719d27962fd6d98a6be6c23
shaik-gousa-khaja-asif <44631692+shaik-gousa-khaja-asif@users.noreply.github.com>
2019-02-01 10:29:09 +0530

    Update container.sh

--- ansible_3par_docker_plugin/container/container.sh
+++ ansible_3par_docker_plugin/container/container.sh
@@ -1,5 +1,5 @@
 docker rmi -f sgkasif/test:1.0
 docker rmi -f container
-docker image build -t container .
+docker image build --no-cache -t container .
 docker tag container sgkasif/test:1.0
 docker push sgkasif/test:1.0
