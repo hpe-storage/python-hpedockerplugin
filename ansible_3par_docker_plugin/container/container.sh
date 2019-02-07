09d90396372e4a6579e878627bfeff15df73b9e6
ansible <ansible@ansible.in.rdlabs.hpecorp.net>
2019-02-07 16:55:14 +0530

    modified container.sh

--- ansible_3par_docker_plugin/container/container.sh
+++ ansible_3par_docker_plugin/container/container.sh
@@ -1,5 +1,5 @@
-docker rmi -f sgkasif/test:1.0
+docker rmi -f sgkasif/hpe3par_docker_volume_plugin_installer:1.0
 docker rmi -f container
 docker image build --no-cache -t container .
-docker tag container sgkasif/test:1.0
-docker push sgkasif/test:1.0
+docker tag container sgkasif/hpe3par_docker_volume_plugin_installer:1.0
+docker push sgkasif/hpe3par_docker_volume_plugin_installer:1.0
