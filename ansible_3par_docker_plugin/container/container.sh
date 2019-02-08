25f446949c4389dab3ac163a2dc964720ad35ef2
Farhan Nomani <farhan7500@gmail.com>
2019-02-08 14:48:36 +0530

    Update container.sh

--- ansible_3par_docker_plugin/container/container.sh
+++ ansible_3par_docker_plugin/container/container.sh
@@ -1,5 +1,5 @@
 docker rmi -f sgkasif/hpe3par_docker_volume_plugin_installer:1.0
 docker rmi -f container
-docker image build --no-cache -t container .
+docker image build --build-arg http_proxy=http://web-proxy.in.hpecorp.net:8080 --build-arg https_proxy=http://web-proxy.in.hpecorp.net:8080 --no-cache -t container .
 docker tag container sgkasif/hpe3par_docker_volume_plugin_installer:1.0
 docker push sgkasif/hpe3par_docker_volume_plugin_installer:1.0
