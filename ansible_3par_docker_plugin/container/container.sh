1db674867b5149f46158967963f3c8b9b7e4848f
asif <asif@asif.asif>
2019-02-20 04:52:38 -0500

    added latest tag

--- ansible_3par_docker_plugin/container/container.sh
+++ ansible_3par_docker_plugin/container/container.sh
@@ -1,5 +1,5 @@
 docker rmi -f hpestorage/legacyvolumeplugininstaller:3.1
 docker rmi -f container
 docker image build --build-arg http_proxy=http://web-proxy.in.hpecorp.net:8080 --build-arg https_proxy=http://web-proxy.in.hpecorp.net:8080 --no-cache -t container .
-docker tag container hpestorage/legacyvolumeplugininstaller:3.1
-docker push hpestorage/legacyvolumeplugininstaller:3.1
+docker tag container hpestorage/legacyvolumeplugininstaller:3.1 hpestorage/legacyvolumeplugininstaller:latest
+docker push hpestorage/legacyvolumeplugininstaller:3.1 hpestorage/legacyvolumeplugininstaller:latest
