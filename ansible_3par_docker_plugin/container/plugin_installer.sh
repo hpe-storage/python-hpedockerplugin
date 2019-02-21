d6c6efd06abd19b5e02b831d2f03c6134699ffb6
asif <asif@asif.asif>
2019-02-21 00:03:54 -0500

    renamed container.sh and parameterized proxy settings

--- ansible_3par_docker_plugin/container/plugin_installer.sh
+++ ansible_3par_docker_plugin/container/plugin_installer.sh
@@ -1,6 +1,6 @@
 docker rmi -f hpestorage/legacyvolumeplugininstaller:3.1
 docker rmi -f container
-docker image build --build-arg http_proxy=http://web-proxy.in.hpecorp.net:8080 --build-arg https_proxy=http://web-proxy.in.hpecorp.net:8080 --no-cache -t hpestorage/legacyvolumeplugininstaller:3.1 -t hpestorage/legacyvolumeplugininstaller:latest .
+docker image build --build-arg http_proxy=$1 --build-arg https_proxy=$2 --no-cache -t hpestorage/legacyvolumeplugininstaller:3.1 -t hpestorage/legacyvolumeplugininstaller:latest .
 #docker tag container hpestorage/legacyvolumeplugininstaller:3.1 
 docker push hpestorage/legacyvolumeplugininstaller:3.1
 docker push hpestorage/legacyvolumeplugininstaller:latest
