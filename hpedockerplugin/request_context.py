0eaeb35ffaa42af2a7fac37f479cc4a31414c034
nilangekarss <swapnil.sun.nilangekar@hpe.com>
2019-07-09 23:10:09 -0700

    Fixed pep8

--- hpedockerplugin/request_context.py
+++ hpedockerplugin/request_context.py
@@ -316,9 +316,10 @@ class FileRequestContextBuilder(RequestContextBuilder):
 
         if fsMode:
             if fsOwner is None:
-                raise exception.InvalidInput("ERROR: If mode bits or "
-                    "directory permissions needs to be changed  then,"
-                    " providing fsOwner is mandetory")
+                raise exception.InvalidInput(
+                    " ERROR: If mode bits or directory permissions"
+                    " needs to be changed  then, providing fsOwner"
+                    " is mandetory")
 
         size_gib = self._get_int_option(options, 'size', 1024)
         # Default share size or quota in MiB which is 1TiB
